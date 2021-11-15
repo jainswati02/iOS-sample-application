dd341502789410abadffdb107e3d4686f74d4058
jainswati02 <jainswati02@gmail.com>
2021-11-14 22:53:37 -0600

    Add files via upload
    sample ios application reproducing dependency cycle error

---
+++ SampleApplication/SampleApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApplication
+//
+//  Created by Swati Jain on 11/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleApplication/SampleApplicationLibrary/SampleApplicationLibrary.swift
@@ -0,0 +1,10 @@
+//
+//  SampleApplicationLibrary.swift
+//  SampleApplicationLibrary
+//
+//  Created by Swati Jain on 11/14/21.
+//
+
+class SampleApplicationLibrary {
+
+}
